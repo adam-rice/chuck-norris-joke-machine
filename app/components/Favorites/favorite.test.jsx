bfa4516cdd3f98198fe8ac75ad1177151487a3cc
Adam Rice <adamcoltonrice@gmail.com>
2017-01-26 22:27:41 -0700

    tests passing Favorites renders as a <div>

--- app/components/Favorites/favorite.test.jsx
+++ app/components/Favorites/favorite.test.jsx
@@ -5,6 +5,18 @@ import { mount, shallow } from 'enzyme';
 import { assert, expect } from 'chai';
 import sinon from 'sinon';
 
+import { Favorites } from './Favorites';
+
+describe('<Favorites/>', () => {
+  it('renders as a <div>', () => {
+    const wrapper = shallow(<Favorites starredJokes={['123']}/>);
+    assert.equal(wrapper.type(), 'div');
+  });
+
+});
+
+
+
 // it('if there is no user there is no welcome message', () => {
 //     const user = false
 //     const wrapper = shallow(<Header user={user}/>)
