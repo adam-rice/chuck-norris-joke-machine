0f85c8a40aa376535e5d3de04186b32e80af89b7
Adam Rice <adamcoltonrice@gmail.com>
2017-01-27 01:15:46 -0700

    settings tested

--- app/components/Settings/settings.test.js
+++ app/components/Settings/settings.test.js
@@ -6,6 +6,7 @@ import { assert, expect } from 'chai';
 import sinon from 'sinon';
 
 import { toggleBool } from './helper';
+import Settings from './Settings';
 
 describe('toggleBool()', () => {
   it('should be a function', () => {
@@ -16,10 +17,56 @@ describe('toggleBool()', () => {
     expect(toggleBool(true)).to.equal(false);
     });
 
-  it('wwill return the true if passed false', () => {
+  it('will return the true if passed false', () => {
     expect(toggleBool(false)).to.equal(true);
     });
 });
 
 describe('<Settings/>', () => {
+  it('renders as a <div>', () => {
+    const wrapper = shallow(<Settings/>);
+
+    assert.equal(wrapper.type(), 'div');
+  });
+
+
+
+  it.skip('should change the prop of parentalControlsDisabled when user clicks pc-On', () => {
+    const wrapper = mount(<Settings parentalControlsDisabled={true}/>);
+
+    wrapper.find('#pc-on').simulate('click');  //id currently removed
+    expect(wrapper.prop('parentalControlsDisabled')).to.equal(false);
+  });
+
+
+
+  it.skip('should change the prop of parentalControlsDisabled when user clicks pc-Off', () => {
+    const wrapper = mount(<Settings parentalControlsDisabled={false}/>);
+
+    wrapper.find('#pc-off').simulate('click');  //id currently removed
+    expect(wrapper.prop('parentalControlsDisabled')).to.equal(true);
+  });
+
+
+
+  it.skip('should change the prop of name when a new name is submitted', () => {
+    const wrapper = mount(<Settings name={''}/>);
+    const event = { target: { value: 'Chris Lane' } };
+
+    wrapper.find('input').simulate('change', event);
+    wrapper.find('set-name-btn').simulate('click');
+    expect(wrapper.prop('name')).to.equal('Chris Lane');
+  });
+
+
+
+  it.skip('should clear the input field when Reset is clicked', () => {
+    const wrapper = mount(<Settings name={''}/>);
+    const event = { target: { value: 'Mark Wahlberg' } };
+    const nameInput = wrapper.find('#name-input');
+
+    nameInput.simulate('change', event);
+    wrapper.find('reset-btn').simulate('click');
+    expect(nameInput.length).to.equal(0);
+  });
 });
