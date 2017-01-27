d32ccd162fe01a5cad1dfa21a4027506e319faad
Adam Rice <adamcoltonrice@gmail.com>
2017-01-26 22:14:57 -0700

    header tests added

--- app/components/Favorites/favorite.test.js
+++ app/components/Favorites/favorite.test.js
@@ -2,5 +2,17 @@
 
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import sinon from 'sinon';
+
+// it('if there is no user there is no welcome message', () => {
+//     const user = false
+//     const wrapper = shallow(<Header user={user}/>)
+//     expect(wrapper.find('p').text()).to.equal('')
+//   })
+
+
+// it('if there is a user the welcome message will render', () => {
+//   const user = true
+//   const wrapper = shallow(<Header user={user}/>)
+//   expect(wrapper.find('p').text()).to.equal('Welcome, ')
--- app/components/Header/header.test.js
+++ app/components/Header/header.test.js
@@ -2,22 +2,25 @@
 
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import sinon from 'sinon';
 
-import Header from './Header';
+import { Header } from './Header';
 
-//
-// describe('<Header/>', () => {
-//   it('should display a single h1 tag', () => {
-//     const wrapper = shallow(<Header/>);
-//     expect(wrapper.find('h1')).to.have.length(1);
-//   });
-//
-//   it('should trigger our logStuff function', () => {
-//     const logStuff = sinon.spy();
-//     const wrapper = mount(<Button handleClick={logStuff} />);
-//     wrapper.simulate('click');
-//     expect(logStuff.calledOnce).to.equal(true);
-//   });
-// });
+describe('<Header/>', () => {
+  it('should display a single h1 tag', () => {
+    const wrapper = shallow(<Header/>);
+    expect(wrapper.find('h1')).to.have.length(1);
+  });
+
+  it('renders as a <div>', () => {
+    const wrapper = shallow(<Header/>);
+    assert.equal(wrapper.type(), 'div');
+  });
+
+  it('the h1 will render an IndexLink', () => {
+    const wrapper = shallow(<Header/>);
+    const header = wrapper.find('h1');
+    expect(header.text()).to.equal('<IndexLink />');
+  });
+});
